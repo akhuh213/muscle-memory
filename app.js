b423ad8b3710d0e67e094b092a18c3be5de2426e
Alice Aekyung Huh <aliceaekyunglee@alices-imac.myfiosgateway.com>
2020-07-17 17:08:46 -0400

    [variables] Add 5 variables

---
+++ app.js
@@ -0,0 +1,17 @@
+//5 variable of different types string, number,array, boolean, object(4 different key/value)
+var string = "I like this song"
+console.log(string);
+var number = 1
+console.log(number);
+var array = [1,2,3]
+console.log(array);
+var isThisBoolean = true;
+console.log(isThisBoolean)
+var object = {
+    food:"sushi",
+    number:3,
+    color:[5,3],
+    clothes:" love this t-shrit",
+    likeIt: true
+};
+console.log(object);
